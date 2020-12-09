0c19575805e8d5b0936b97248b8a68b331be2d1b
lordanb <gabriel.massukado@hotmail.com>
2020-12-08 22:14:01 -0300

    Primeiro script de deletar decorações

---
+++ Assets/Resources/Scripts/AreaChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaChecker : MonoBehaviour
+{ 
+    [SerializeField] private LayerMask worldLayer;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & worldLayer) != 0)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
